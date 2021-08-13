220139165ac1f1ef6089be8ab18eea39b8a9a16f
kushagragpt99 <glaedur@inheritance.localdomain>
2021-08-13 19:12:55 +0530

    changes to init.c

--- src/init.c
+++ src/init.c
@@ -13,6 +13,10 @@ extern SEXP _mcmcse_mbmC(SEXP, SEXP);
 extern SEXP _mcmcse_mobmC(SEXP, SEXP);
 extern SEXP _mcmcse_msveC(SEXP, SEXP, SEXP);
 extern SEXP _mcmcse_lag(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _mcmcse_batchsize_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _mcmcse_mcseqbm(SEXP, SEXP, SEXP);
+extern SEXP _mcmcse_mcseqobm(SEXP, SEXP, SEXP);
+extern SEXP _mcmcse_mcseqsub(SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
     {"_mcmcse_inseq", (DL_FUNC) &_mcmcse_inseq, 2},
@@ -20,6 +24,10 @@ static const R_CallMethodDef CallEntries[] = {
     {"_mcmcse_mobmC",  (DL_FUNC) &_mcmcse_mobmC,  2},
     {"_mcmcse_msveC", (DL_FUNC) &_mcmcse_msveC, 3},
     {"_mcmcse_lag", (DL_FUNC) &_mcmcse_lag, 4},
+    {"_mcmcse_batchsize_cpp", (DL_FUNC) &_mcmcse_batchsize_cpp, 6},
+    {"_mcmcse_mcseqbm", (DL_FUNC) &_mcmcse_mcseqbm, 3},
+    {"_mcmcse_mcseqobm", (DL_FUNC) &_mcmcse_mcseqobm, 3},
+    {"_mcmcse_mcseqsub", (DL_FUNC) &_mcmcse_mcseqsub, 3},
     {NULL, NULL, 0}
 };
 
