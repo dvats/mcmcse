035ebd066f173b13d4cd103a5ffbb298347f6bc4
kushagragpt99 <glaedur@inheritance.localdomain>
2021-08-19 18:32:06 +0530

    tukey is annoying me

--- R/mcmcse.R
+++ R/mcmcse.R
@@ -2,8 +2,8 @@
 #' Compute Monte Carlo standard errors for expectations. 
 #'
 #' @param x a vector of values from a Markov chain of length n.
-#' @param size represents the batch size in \dQuote{\code{bm}} and the truncation point in \dQuote{\code{bartlett}} and \dQuote{\code
-#' {tukey}}. Default is \code{NULL} which implies that an optimal batch size is calculated using the 
+#' @param size represents the batch size in \dQuote{\code{bm}} and the truncation point in \dQuote{\code{bartlett}} and 
+#' \dQuote{\code{tukey}}. Default is \code{NULL} which implies that an optimal batch size is calculated using the 
 #'   \code{batchSize} function. Can take character values of \dQuote{\code{sqroot}} and \dQuote{\code{cuberoot}} or any numeric
 #'   value between 1 and n/2. \dQuote{\code{sqroot}} means size is floor(n^(1/2)) and \dQuote{\code{cuberoot}} means size is
 #'   floor(n^(1/3)).
@@ -92,8 +92,8 @@ mcse <- function(x, size = NULL, g = NULL, r=3, method = c("bm", "obm", "bartlet
 #' Apply \code{mcse} to each column of a matrix or data frame of MCMC samples.
 #'
 #' @param x a matrix of values from a Markov chain of size n x p.
-#' @param size represents the batch size in \dQuote{\code{bm}} and the truncation point in \dQuote{\code{bartlett}} and \dQuote{\code
-#' {tukey}}. Default is \code{NULL} which implies that an optimal batch size is calculated using the 
+#' @param size represents the batch size in \dQuote{\code{bm}} and the truncation point in \dQuote{\code{bartlett}} and 
+#' \dQuote{\code{tukey}}. Default is \code{NULL} which implies that an optimal batch size is calculated using the 
 #'   \code{batchSize} function. Can take character values of \dQuote{\code{sqroot}} and \dQuote{\code{cuberoot}} or any numeric
 #'   value between 1 and n/2. \dQuote{\code{sqroot}} means size is floor(n^(1/2)) and \dQuote{\code{cuberoot}} means size is
 #'   floor(n^(1/3)).
